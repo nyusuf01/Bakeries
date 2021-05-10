b000027e3bea5d373efd842ade41990ba29c37bf
nyusuf01 <nyusuf0001@gmail.com>
2021-05-10 08:35:37 -0400

    first api call

--- bakery/src/App.js
+++ bakery/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { baseURL, config } from "./services";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [bakeries, setBakeries] = useState([]);
+
+  useEffect(() => {
+    const fetchBakeries = async () => {
+      const resp = await axios.get(baseURL, config);
+      console.log(resp.data);
+    };
+    fetchBakeries();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
